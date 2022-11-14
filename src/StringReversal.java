263180d6e6d0b2c786f27c480f331ce78ca72409
Rakesh R <rakeshradharamanan@gmail.com>
2022-11-14 17:03:53 -0600

    Using StringBuilder instead of "+" (concatenate) and build the string.

--- src/StringReversal.java
+++ src/StringReversal.java
@@ -1,5 +1,19 @@
-public class Main {
+public class StringReversal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String inputString = "I am a bad boy!!";
+        String reversedString = reverse(inputString);
+        System.out.println("Your input string : "+inputString);
+        System.out.println("Your reversed string : "+reversedString);
+    }
+
+    public static String reverse(String str){
+        StringBuilder sb = new StringBuilder();
+
+        //String reverse = "";
+        for (int i = str.length()-1; i >=0 ; i--) {
+            sb.append(str.charAt(i));
+
+        }
+        return sb.toString();
     }
 }
