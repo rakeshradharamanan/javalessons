a67d4546ab5f3001304e9f16f00d7b22b5a4f421
Rakesh R <rakeshradharamanan@gmail.com>
2022-11-14 17:09:42 -0600

    Formatted the code.

--- src/StringReversal.java
+++ src/StringReversal.java
@@ -2,17 +2,14 @@ public class StringReversal {
     public static void main(String[] args) {
         String inputString = "I am a bad boy!!";
         String reversedString = reverse(inputString);
-        System.out.println("Your input string : "+inputString);
-        System.out.println("Your reversed string : "+reversedString);
+        System.out.println("Your input string : " + inputString);
+        System.out.println("Your reversed string : " + reversedString);
     }
 
-    public static String reverse(String str){
+    public static String reverse(String str) {
         StringBuilder sb = new StringBuilder();
-
-        //String reverse = "";
-        for (int i = str.length()-1; i >=0 ; i--) {
+        for (int i = str.length() - 1; i >= 0; i--) {
             sb.append(str.charAt(i));
-
         }
         return sb.toString();
     }
